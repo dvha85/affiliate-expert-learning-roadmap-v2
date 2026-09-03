a1902a9f9e6a60d565061ef0a7fce4fb0116bd45
Đinh Hà <hadinhvu@gmail.com>
2026-09-03 15:21:14 +0700

    Expose M10 demo in CLI

--- lab/mission-runtime/cmd/demo/main.go
+++ lab/mission-runtime/cmd/demo/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "usage: go run ./cmd/demo O00|M03|M04|M05|M06|M07|M08|M09")
+		fmt.Fprintln(os.Stderr, "usage: go run ./cmd/demo O00|M03|M04|M05|M06|M07|M08|M09|M10")
 		os.Exit(2)
 	}
 	if err := runMissionDemo(os.Args[1:]); err != nil {
